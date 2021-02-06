9be1d8e8ceeb2dc56cd7c49e6570408a0d2b2907
Ricardo Pineda Lazcano <urielink81@gmail.com>
2021-02-05 19:27:12 -0600

    test wide, algo me falta declarar.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-server-side-render'), 'version' => 'c446ed10526466efd38738d07147d0e9');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-server-side-render'), 'version' => 'b0e6615f3412f953849838968e50ba1d');
