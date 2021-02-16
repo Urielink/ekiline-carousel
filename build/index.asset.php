c931c49371310de3a57f5d34728cb3898f42f914
Ricardo Pineda Lazcano <urielink81@gmail.com>
2021-02-16 10:34:38 -0600

    textos ok

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-server-side-render'), 'version' => 'dfb5f805a1d09bfda9b4e7e3d17704f3');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-server-side-render'), 'version' => '64760733688779a4a259bd894d15a577');
